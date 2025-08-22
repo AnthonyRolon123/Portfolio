7b486839a92794ec4ef2ca237028703681e53103
Anthony <anthonyrolon1234@gmail.com>
2025-08-22 18:19:40 -0400

    added analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,12 @@
 import '../globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
                 {children}
+                <Analytics/>
             </body>
         </html>
     )
